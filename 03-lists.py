89baa06b60430ef90d02d9b3fa32506c6fbb2b35
Ranjeet Kumar Pandit <ranjeet.kumar.pandit@sap.com>
2025-08-13 14:23:14 +0000

    List and nested List

--- 03-lists.py
+++ 03-lists.py
@@ -1,3 +1,10 @@
 fruits = ["mango","apple","guava"]
 print(fruits)
 print(fruits[1])# index starts at 0
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
