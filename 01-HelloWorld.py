fc6484508d9f789488dc631bde15468d1705e313
Ranjeet Kumar Pandit <ranjeet.kumar.pandit@sap.com>
2025-08-13 12:08:09 +0000

    Input and String Learning

--- 01-HelloWorld.py
+++ 01-HelloWorld.py
@@ -1,1 +1,12 @@
 print('Hello World')
+#String manipulation
+print("Hello World\nHello World!")
+print("Hello " + "Ranjeet")
+#Be aware of indentation error
+
+
+#Input () used to collect user input & input always return String 
+name = input("What is your name?")
+print("Hello "+name)
+#Find the length of a string
+print("Length of name is ", len(name))
