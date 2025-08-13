45eb209d18c7060d98a6d822466cc7072c7775f9
Ranjeet Kumar Pandit <ranjeet.kumar.pandit@sap.com>
2025-08-13 14:42:10 +0000

    Loops and functions

---
+++ 04-Loop.py
@@ -0,0 +1,12 @@
+fruits = ["apple","mango","guava"]
+for items in fruits:
+    print(items)
+
+
+#Range: range works with function like for loop, it can not work alone
+sum = 0 
+for number in range(1,10):
+    print(number)
+    sum=sum+number
+
+print(sum)
---
+++ 05-functions.py
@@ -0,0 +1,4 @@
+#def my_function():
+
+#calling function: my_function()
+
