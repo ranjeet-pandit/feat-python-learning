226cc1598b244e343da78299c4985d7922b4cbf9
Ranjeet Kumar Pandit <ranjeet.kumar.pandit@sap.com>
2025-08-14 14:30:02 +0000

    class objects

--- 05-functions.py
+++ 05-functions.py
@@ -1,4 +1,11 @@
 #def my_function():
 
 #calling function: my_function()
+import random
+def my_fun():
+    fruits = ["apple","guava","carrot","pineapple"]
 
+    chose_fruits = random.choice(fruits)
+    print(chose_fruits)
+
+my_fun()
---
+++ main.py
@@ -0,0 +1,6 @@
+# We can check for existing package in PyPI website
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon",["Apple","Guava"])
+
+print(table)
